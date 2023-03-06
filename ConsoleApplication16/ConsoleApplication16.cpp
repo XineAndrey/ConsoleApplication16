c7e751f635a826d432e5abfd3d4de8a1dcb825ec
XineAndrey <xyligan4ek.x1ne@mail.ru>
2023-03-06 21:08:59 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication16/ConsoleApplication16.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <string>
+
+
+using namespace std;
+
+
+
+int main()
+{
+    srand(time(0));
+    setlocale(LC_ALL, "ru");
+
+    cout << "Hello World";
+
+}
+
+
